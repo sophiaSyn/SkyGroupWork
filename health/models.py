fc39593a47d997d23aa4da4dc92bf73949e1b22d
sophiaSyn <bari3310@gmail.com>
2025-04-20 23:00:50 +0100

    Updated Models for the Health App
    Added return values so that they show up correctly rather than as ID numbers on the Django Admin Page

--- health/models.py
+++ health/models.py
@@ -15,6 +15,9 @@ class Team(models.Model):
     teamName = models.CharField(max_length=50, unique=True)
     departmentNo = models.ForeignKey(Department, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.teamName
+
 class Session(models.Model):
     sessionID = models.AutoField(primary_key=True)
     startDate = models.DateField()
@@ -25,21 +28,36 @@ class Session(models.Model):
     teamID = models.ForeignKey(Team, on_delete=models.CASCADE)
     departmentNo = models.ForeignKey(Department, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Session {self.sessionID} - {self.startDate} to {self.endDate}"
+    
 class State(models.Model):
     stateID = models.AutoField(primary_key=True)
     stateColour = models.CharField(max_length=5)
 
+    def __str__(self):
+        return self.stateColour
+    
 class Trend(models.Model):
     trendID = models.AutoField(primary_key=True)
     trendStatus = models.CharField(max_length=15)
 
+    def __str__(self):
+        return self.trendStatus 
+    
 class Card(models.Model):
     cardID = models.AutoField(primary_key=True)
     cardName = models.CharField(max_length=50)
-
+    
+    def __str__(self):
+        return self.cardName
+    
 class Vote(models.Model):
     voteID = models.AutoField(primary_key=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     sessionID = models.ForeignKey(Session, on_delete=models.CASCADE)
     stateID = models.ForeignKey(State, on_delete=models.CASCADE)
     trendID = models.ForeignKey(Trend, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Vote {self.voteID} - Session {self.sessionID.sessionID}"
